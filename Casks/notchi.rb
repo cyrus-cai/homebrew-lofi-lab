74e9b21bc911e7430f88072ebbabf836cab848fa
Cyrus Cai <andy864940012@gmail.com>
2026-08-01 18:56:25 +0800

    notchi 0.4.2

--- Casks/notchi.rb
+++ Casks/notchi.rb
@@ -1,6 +1,6 @@
 cask "notchi" do
-  version "0.4.0"
-  sha256 "0f850d01b57d71d86f3b585abb08a9cad3587eb74a03b19f11bf9741e97097f5"
+  version "0.4.2"
+  sha256 "f6d39c6b08a924fac4d74b0e6f5fc54f7d435e7661bd8cc2bdc9fac8225e366f"
 
   url "https://github.com/cyrus-cai/notchi/releases/download/v#{version}/Notchi-v#{version}.zip"
   name "Notchi"
